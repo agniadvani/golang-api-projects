33ba20f6eff0e18bfc869c08563b2c32c9816b56
agniadvani <advani.agni.agni@gmail.com>
2023-04-17 23:30:16 +0530

    Movie-APP

--- 1.go-movies-crud/main.go
+++ 1.go-movies-crud/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -37,4 +40,65 @@ func main() {
 	fmt.Println("Starting the server at PORT 8000")
 
 	log.Fatalln(http.ListenAndServe(":8000", r))
+}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for i, m := range movies {
+		if params["id"] == m.ID {
+			movies = append(movies[:i], movies[i+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for _, m := range movies {
+		if m.ID == params["id"] {
+			json.NewEncoder(w).Encode(m)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000))
+	fmt.Printf("Movie: %+v", movie)
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for i, m := range movies {
+		if params["id"] == m.ID {
+			movies = append(movies[:i], movies[i+1:]...)
+
+			var movie Movie
+
+			json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(10000))
+			movies = append(movies, movie)
+			return
+		}
+	}
 }
